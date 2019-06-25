7699694125dbaf05a249fd792ed3fcd76280d76b
Fabio <pancotti@bol.com.br>
2019-06-25 20:17:41 -0300

    atualizando para heroku

---
+++ AULA 109.py
@@ -0,0 +1,15 @@
+#AULA 109 - DEPLOY DA APLICAÇÃO
+
+#Cria um usuário do HEROKU - http://heroku.com
+
+#Usuário: pancotti@bol.com.br
+#Password: Bb981920
+
+#No Prompt do MSDOS, dentro da pasta de trabalho, digita os comandos:
+#heroku login. O sistema irá pedir a senha através do site
+
+#No pycharm deve ser criado um arquivo chamado |Procfile. Neste arquivo deve conter a lista de comando para iniciar
+#cada processo.
+#
+#
+#
